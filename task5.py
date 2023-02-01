e16d46200fdd79d825378d93024874e2c690f468
Danis Sharipov <danshadance@mail.ru>
2023-02-01 10:09:33 +0600

    task 5 upd

--- task5.py
+++ task5.py
@@ -4,13 +4,18 @@
 
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
 
+print()
+print("Данная программа составляет список чисел Фибоначчи, в том числе для отрицательных индексов.")
+print()
+user_k = int(input("Введите, пожалуйста, число: "))
+
 fibo = [0,1]
 
 count = 0
 a = int(0)
 b = int(1)
 
-while count < 7:
+while count < user_k - 1:
     temp = b
     b = a + b
     app = fibo.append(b)
@@ -21,18 +26,16 @@ index = 1
 counter = 1
 j = 0
 
-while counter < 9:
+while counter < user_k + 1:
     if counter % 2 != 0:
-        print(fibo[index])
         fibo.insert(j, fibo[index])
         index += 2
         counter += 1
     else:
-        print(fibo[index])
         minus = fibo[index] * (-1)
         fibo.insert(j, minus)
         index += 2
         counter += 1
 
 print()
-print(fibo)
+print("Ответ: ",fibo)
